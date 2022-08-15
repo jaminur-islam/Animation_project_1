adadb9e51255a71e8d2bb2d55e61bfc4b8299e9d
jaminur islam <jaminurislam350@gamil.com>
2022-08-15 13:03:13 +0600

    love add

--- canvas.js
+++ canvas.js
@@ -82,7 +82,7 @@ let circleArray = [];
 
 const init = () => {
   circleArray = [];
-  for (let i = 0; i < 2000; i++) {
+  for (let i = 0; i < 1000; i++) {
     let radius = Math.random() * 3 + 1;
     let x = Math.random() * (innerWidth - radius * 2) + radius;
     let y = Math.random() * (innerHeight - radius * 2) + radius;
